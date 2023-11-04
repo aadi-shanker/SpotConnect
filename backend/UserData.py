fa3d54f7524c21e3537f3789330452ba05a55ec9
Han Seo Ro <hsro@uci.edu>
2023-11-04 15:25:15 -0700

    made new endpoint

--- backend/UserData.py
+++ backend/UserData.py
@@ -1,11 +1,17 @@
-def store_user_data(user_token):
+import DatabaseHandler
+
+def store_user_data(database: DatabaseHandler, user_token):
     """Given a user's spotify token, retrieves the user's data from
     Spotify API and stores data in database"""
     #check if user already exists
 
     #if user doesn't exist, create new user and insert their data
+
+
     #store user's name, dob, social media handle, top 30 tracks, top 5 artists, top 5 genres,
     #top 10 songs, top 5 albums
+
+
     raise NotImplemented
 
 
@@ -15,6 +21,5 @@ def retrieve_top_30_tracks(user_token):
     raise NotImplemented
 
 def calculate_match(person1: dict, person2: dict) -> bool:
-    """Given two users, accesses the database to retrieve the user's
-    listening data and uses that data to determine their compatibility"""
+    """Given two persons, uses their data to calculate compatibility"""
     raise NotImplemented
