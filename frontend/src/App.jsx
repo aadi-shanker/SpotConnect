d89502e6ef04a057b9a2affcd41f85253135bdbd
kylebtran <ucitrankyl@gmail.com>
2023-11-04 16:06:13 -0700

    connected login page to social input page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,14 @@
 import {useEffect, useState} from "react";
 import "./App.css";
 import Zotify from "./assets/Zotify.svg";
+import {Route, Router} from "react-router-dom"
+import LoginPage from "./Components/LoginPage";
 
 
 function App() {
-    const CLIENT_ID = "257664458a0b4da38b7d2fb3898c059f"
-    const REDIRECT_URI = "http://localhost:5173"
-    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-    const RESPONSE_TYPE = "token"
-    const SPOTIFY_AUTH = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
-
-
-    const [token, setToken] = useState("")
-
-    function redirectToSpotify(){
-        window.open(SPOTIFY_AUTH)
-    }
-
-    useEffect(() => {
-        const hash = window.location.hash
-        let token = window.localStorage.getItem("token")
-
-        if (!token && hash) {
-            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-            window.location.hash = ""
-            window.localStorage.setItem("token", token)
-        }
-
-        setToken(token)
-
-    }, [])
-
-    const logout = () => {
-        setToken("")
-        window.localStorage.removeItem("token")
-    }
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <button onClick={redirectToSpotify} className="Button">Login with Spotify</button>
-          </header>
-      </div>
-  );
+        <LoginPage/>
+    )
 }
 
 
---
+++ frontend/src/Components/LoginPage.jsx
@@ -0,0 +1,45 @@
+import {useEffect, useState} from "react";
+
+
+function LoginPage() {
+    const CLIENT_ID = "257664458a0b4da38b7d2fb3898c059f"
+    const REDIRECT_URI = "http://localhost:5173/social"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+    const SPOTIFY_AUTH = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+
+
+    const [token, setToken] = useState("")
+
+    function redirectToSpotify(){
+        window.open(SPOTIFY_AUTH)
+    }
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    return (
+        <button onClick={redirectToSpotify} className="Button">Login with Spotify</button>
+
+  );
+}
+
+
+export default LoginPage;
