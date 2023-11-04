a945b536e6ad771667c868f8de1a60f2084f7a4b
kylebtran <146040347+kylebtran@users.noreply.github.com>
2023-11-04 15:00:41 -0700

    added spotify auth and zotify logo. (#3)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,50 @@
-import React, { useState } from "react";
-import SocialMediaInput from "./Components/socialMediaInput";
+import {useEffect, useState} from "react";
+import "./App.css";
+import Zotify from "./assets/Zotify.svg";
+
 
 function App() {
-
-  const [socialMedia, setSocialMedia] = useState('');
-
-  const handleSocialMediaChange = (e) => {
-    setSocialMedia(e.target.value);
-  };
-
-  // Inline style object for text alignment
-  const textStyle = {
-    color: 'green',
-    textAlign: 'center',
-  };
+    const CLIENT_ID = "257664458a0b4da38b7d2fb3898c059f"
+    const REDIRECT_URI = "http://localhost:5173"
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    const RESPONSE_TYPE = "token"
+    const SPOTIFY_AUTH = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
 
-  return (
-    
-    <SocialMediaInput/>
+    const [token, setToken] = useState("")
+
+    function redirectToSpotify(){
+        window.open(SPOTIFY_AUTH)
+    }
+
+    useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+            window.location.hash = ""
+            window.localStorage.setItem("token", token)
+        }
+
+        setToken(token)
+
+    }, [])
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={redirectToSpotify} className="Button">Login with Spotify</button>
+          </header>
+      </div>
   );
 }
+
 
 export default App;
