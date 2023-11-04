81e844c9b64527d2be75cc1c6795abedfcac1563
kylebtran <146040347+kylebtran@users.noreply.github.com>
2023-11-04 16:07:39 -0700

    connected login page to social input page. (#6)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  Router,
+  RouterProvider
+} from "react-router-dom"
+import LoginPage from "./Components/LoginPage.jsx";
+import SocialMediaInput from "./Components/SocialMediaInput.jsx"
 
-/*
-This code renders our project so it can be viewed in a browser. 
-*/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage/>
+  },
+  {
+    path: "/social",
+    element: <SocialMediaInput/>
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
