8d91d0b85d4a196929ae100c3d7274c90c8f8b93
Han Seo Ro <hsro@uci.edu>
2023-11-04 19:43:58 -0700

    fixed logic in main function

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,6 @@ from typing import Optional
 
 import uvicorn
 from fastapi import FastAPI, status
-from numpy.random import randint
 
 from DatabaseHandler import DatabaseHandler
 import UserData
@@ -63,45 +62,42 @@ def main(access_token):
 
     #store/update user's data
     UserData.store_user_data(users_database, access_token)
+    users_id = UserData.user_id(users_database, access_token)
 
-    #retrieve person1 data
-    person1 = users_database.get_data("_id", )
-
-    #select a random user
-    #get max id
-    maxID = users_database.get_max_id()
-
-    #generate random id under max id
-    randomID = randint(0,maxID)
-
-    #retrieve that person2 data and store as dict
-    person2 = users_database.get_data("_id", randomID)
+    person1 = users_database.get_data("_id", users_id)
+    person2 = UserData.get_random_person(users_database,users_id)
 
     #process both users top 50 tracks and calculate if they are a match
-    UserData.calculate_match(person1, person2)
+    while not UserData.calculate_match(person1, person2):
+        person2 = UserData.get_random_person(users_database, users_id)
 
-
+    #now person1 and person 2 is a match
+    print("Match found")
 
     return {"message": "This is the main page"}
 
 
 # TODO: Add POST route for demo
 
+#for testing purposes only
 if __name__ == "__main__":
     #uvicorn.run("main:app", port=5000, reload=True)
 
-    temp_access_token = 'BQCVQNtanDouNIVEBU6RkQTITR_BlcYe5hu5EPup4mfPZ2LMlUgFlWWuem-xyhBeX-7mvaxHII8rVAU9i0GfrQxU9jTPD2v2v0SOOLdGUMi93h3P0Xngh8kNn1mx9EU2EsEhnZQBGkGuRTgjQ7ziXeSetKnxjzXCXJQeOQWKo6ljCtD5mFZMsBIZPNrJnho_M0c'
+    temp_access_token = 'BQDkm22-8ng3ilRIb_IplHw-FCuLshouylXjnswLnr1Ewlv2rS5knsWdqhLUxTdFXnYU9vNjhS743fR0ahxJd1jBZlSFNSyheDSpHt6H83b5iuArat4rSKuxqM2ivbS2pNvvNzpkbQIZFssLeOJLNbd46G9UEEHfaHmDMeYisdorTuuBmum2VL4Lr0dAcD39ucTPdGN4xlLCJEpqlwITATUZ'
 
     users_database = DatabaseHandler("Users")
-    #sampleData = {"_id": 0, "name": "Bob Smith", "spotify_name": "Han Seo", "date of birth": "23/01/2001"
-    #              "top 30 tracks": [], "top 5 artists":[], "top 5 genres":[],
-    #              "top 10 songs": [], "top 5 albums":[]}
 
     #users_database.insert_data(sampleData)
 
     #person1 = users_database.get_data("name","Bob Smith")
     #print(person1)
     #print(type(person1))
-    UserData.find_all_listening_data(temp_access_token)
 
+    UserData.store_user_data(users_database,temp_access_token)
 
+    # tempdict = UserData.find_all_listening_data(temp_access_token)
+    # print(tempdict["top_tracks"])
+    # for x in tempdict.items():
+    #     print(x)
+    #
+    #
